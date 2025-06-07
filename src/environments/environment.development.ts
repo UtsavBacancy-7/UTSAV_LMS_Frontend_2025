0cbe3bc9dee89ceeb4d6a55f76baed690aaadd0b
Utsav Katharotiya <utsav.katharotiya@bacancy.com>
2025-06-07 11:02:56 +0530

    In Progress: Initial Commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    production: false,
+
+    apiBaseUrl: 'https://localhost:5001/api',
+
+    appName: 'Readify - Library Management System',
+
+    authTokenKey: 'library_auth_token',
+
+    roles: {
+        admin: 'Admin',
+        librarian: 'Librarian',
+        student: 'Student'
+    }
+};
